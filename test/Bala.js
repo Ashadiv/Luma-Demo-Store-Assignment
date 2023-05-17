58de5b7ace3728b0097ce824cb7faca1ded16a51
balavadivel82@gmail.com <balavadivel82@gmail.com>
2023-05-17 11:59:51 +0200

    commiting the test case of sort by Price

--- test/Bala.js
+++ test/Bala.js
@@ -1,5 +1,6 @@
+// Implement a test of newsletter subscription
 // Import Builder, By, Key and Until from the selenium library
-const { Builder, By, Key, Until } = require("selenium-webdriver");
+const { Builder, By, Key, until, WebElement } = require("selenium-webdriver");
 
 //Import different assert from chai
 
@@ -28,5 +29,41 @@ describe("Implement a test of newsletter subscription", () => {
       .findElement(By.className("messages"))
       .getText();
     expect(subscriptionMsg).to.exist;
+  });
+});
+
+// Implement a test of Sort by
+describe("Implement a test of Sort By", () => {
+  it("Successfully implement a test case of Sort By", async () => {
+    // Start the webdriver and go the web page
+    let driver = await new Builder().forBrowser("firefox").build();
+    await driver.get("https://magento.softwaretestingboard.com");
+    //select the menu for Men
+    driver.findElement(By.linkText("Men")).click();
+    //wait until next menu is loaded
+    await driver.wait(until.elementLocated(By.linkText("Tops")), 10000);
+    //select the menu for Tops
+    driver.findElement(By.linkText("Tops")).click();
+    //wait until next menu is loaded
+    await driver.wait(until.elementLocated(By.linkText("Jackets")), 10000);
+    //select the menu for Jackets
+    driver.findElement(By.linkText("Jackets")).click();
+    //wait until next menu is loaded
+    await driver.wait(until.elementLocated(By.id("sorter")), 10000);
+    //select the Dropdown
+    await driver.findElement(By.id("sorter")).click();
+    //wait
+    await driver.sleep(1000);
+    //select the option Price
+    await driver.findElement(By.id("sorter")).sendKeys("Price");
+
+    //wait
+    await driver.sleep(2000);
+    //get the selected dropdown value
+    var optionVal = await driver
+      .findElement(By.xpath("//select[@id='sorter']//option[@selected]"))
+      .getText();
+    //check if the selected value is equal to what is send
+    expect(optionVal).to.equal("Price");
   });
 });
